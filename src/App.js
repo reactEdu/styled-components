b3f28ec93b2b752cb4f97c56f18c3ebac3317f62
macbook_of_L <ds2lvg@gmail.com>
2019-12-26 13:47:07 +0900

    attrs, css헬퍼

--- src/App.js
+++ src/App.js
@@ -5,60 +5,40 @@ import styled, {createGlobalStyle, css, keyframes} from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   *{padding:0; margin:0; font-size: 14px;}
-`
-
-class App extends Component {
-  render() {
-    return (
-      <Container>
-        <GlobalStyle />
-        <Button success>success</Button>
-        <Button danger rotationTime={5}>danger</Button>
-        <Anchor href="https://www.styled-components.com/docs/api">styled-components.com</Anchor>
-      </Container>
-    );
-  }
-}
+`;
 
 const Container = styled.div`
   height: 100vh;
   width: 100%;
   background-color: pink;
-`
-
-const Button = styled.button`
-  border-radius: 50px;
-  padding: 5px;
-  min-width: 120px;
-  color: white;
-  font-weight: 600;
-  -webkit-appearance: none;
-  cursor: pointer;
-  &active,
-  &focus{
-    outline: none;
-  }
-  background-color: ${props => props.danger ? '#e74c3c' : '#2ecc71'}
-  /* return시 css 헬퍼와 함께 사용 */
-  ${props => {
-    if(props.danger) {
-      return css`animation: ${rotation} ${props.rotationTime}s linear infinite`;
-    }
-  }}
 `;
 
-const rotation = keyframes`
-  from{
-    transform: rotate(0deg);
-  }
-  to{
-    transform: rotate(360deg);
-  }
+const awesomeCard = css`
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
 `;
 
-// Button 컴포넌트 스타일을 같게 적용하는 a태그를 만들고 css 추가
-const Anchor = styled(Button.withComponent("a"))`
-  text-decoration:none;
+// setAttribute() 기능을 해주는 attrs()
+const Input = styled.input.attrs({
+  type: "password",
+  required: true
+})`
+  border: none;
+  ${awesomeCard};
 `;
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Input placeholder="hello" />
+        <GlobalStyle />
+      </Container>
+    );
+  }
+}
+
 
 export default App;

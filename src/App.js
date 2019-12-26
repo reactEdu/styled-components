df9fdfb678235e661b508bd145fcd7dd7705e558
macbook_of_L <ds2lvg@gmail.com>
2019-12-26 14:36:54 +0900

    ThemeProvider과 nesting

--- src/App.js
+++ src/App.js
@@ -1,44 +1,50 @@
-import React, { Component, Fragment } from "react";
-import styled, {createGlobalStyle, css, keyframes} from "styled-components";
-// injectGlobal 없어짐
-// https://www.styled-components.com/docs/api#createglobalstyle
+import React, { Component } from "react";
+import styled, {createGlobalStyle, ThemeProvider} from "styled-components";
+import theme from './theme';
 
 export const GlobalStyle = createGlobalStyle`
   *{padding:0; margin:0; font-size: 14px;}
+`;
+
+const Card = styled.div`
+  background-color: red;
 `;
 
 const Container = styled.div`
   height: 100vh;
   width: 100%;
   background-color: pink;
+  ${Card}:first-child { 
+    /* 카드는 원래 빨간색이지만 첫번째 자식은 파란색으로 변경 */
+    background-color: blue;
+  }
 `;
 
-const awesomeCard = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  background-color: white;
-  border-radius: 10px;
-  padding: 20px;
-`;
-
-// setAttribute() 기능을 해주는 attrs()
-const Input = styled.input.attrs({
-  type: "password",
-  required: true
-})`
-  border: none;
-  ${awesomeCard};
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  /* 5단계 아래있는 버튼에서 theme에 한번에 접근가능 */
+  background-color: ${props => props.theme.successColor};
 `;
 
 class App extends Component {
   render() {
     return (
-      <Container>
-        <Input placeholder="hello" />
-        <GlobalStyle />
-      </Container>
+      // 하위 컴포넌트들에서 theme에 있는 속성들 사용 가능
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Form />
+          <Form />
+          <GlobalStyle />
+        </Container>
+      </ThemeProvider>
     );
   }
 }
 
+const Form = () => (
+<Card>
+  <Button>Hello</Button>
+</Card>);
 
 export default App;
